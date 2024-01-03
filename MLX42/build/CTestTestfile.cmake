648f43bcfb0371820df0a2ecb76c2ac7e06e3311
El mehdi Agoumi <eagoumi@e3r1p1.1337.ma>
2024-01-03 01:56:26 +0100

    Pars

---
+++ MLX42/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/eagoumi/Desktop/cub3D/MLX42
+# Build directory: /Users/eagoumi/Desktop/cub3D/MLX42/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
