72e9c7d41ba3484d20d2915c514ca381cc5b11a0
El mehdi Agoumi <eagoumi@e3r1p1.1337.ma>
2024-01-04 04:05:02 +0100

    Pars

--- teststrnstr.c
+++
@@ -1,8 +0,0 @@
-#include <libc.h>
-
-int main (void)
-{
-    char * tmp;
-    // tmp = strnstr("Agoumi El Mehdi", "Mehdi", 3);
-    printf("%s\n", strnstr("Agoumi El Mehdi", "E", strlen("Agoumi El Mehdi")));
-}
